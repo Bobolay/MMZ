ddaf17d62c66a33ddb2d6d4c360b2acf40950e8a
Bobolay <bogdan4uk@gmail.com>
2016-04-21 15:51:49 +0300

    first

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  root to: "pages#index"
+
+  controller :pages do
+    get "about-us", action: "about_us"
+  end
+
+  # mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  # mount Ckeditor::Engine => '/ckeditor'
+  #devise_for :users
+
+
+  match "*url", to: "application#render_not_found", via: [:get, :post, :path, :put, :update, :delete]
+end
